2b2b709de3bf5f6ea8b6e95206f48f833761acf7
黄海彬 <huanghaibin@followme-inc.com>
2019-02-14 10:23:17 +0800

    chore: init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'i18n-tool'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
